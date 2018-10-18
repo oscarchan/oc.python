1b80feb63488e7131f3188c790390edc49bb32bb
Oscar Chan <oscarc@surveymonkey.com>
2018-10-18 15:56:17 -0700

    converted functions.py to pytest format

--- functions.py
+++ functions.py
@@ -1,69 +1,122 @@
+import pytest
 '''  
    Calling functions
 '''
-print
-print "---- Example: formal and keyword arguments ----"
-print
+
 def echo(*args, **kwargs):
-    print "*args", args
-    print "**kwargs", kwargs
+    """---- Example: formal and keyword arguments ----"""
+    return {
+        "*args=": str(args),
+        "**kwargs": str(kwargs)
+        }
 
 def echoKwargsOnly(**kwargs):
-    print "**kwargs", kwargs
+    return {
+        "**kwargs": str(kwargs)
+    }
 
-print ">> echo(1, 2, 3, 4)"
-echo(1, 2, 3, 4)
+def test_formal_args():
+    assert echo(1, 2, 3, 4) == {
+        "*args=": str( (1, 2, 3, 4) ),
+        "**kwargs": str( {} )
+    }
+        
 
-print ">> echo(1, 2, three=3, four=4)"
-echo(1, 2, three=3, four=4)
+def test_kw_args():
+    assert echo(1, 2, three=3, four=4) == ""
 
-print '>> echo([1, 2, 3], {"four"=4, "five"=5})'
-echo([1, 2, 3], {"four": 4, "five": 5})
+def test_formal_kw_args():
+    assert echo([1, 2, 3], {"four": 4, "five": 5}) == ""
 
-print '>> echo(*[1, 2, 3], **{"four": 4, "five": 5})'
-echo(*[1, 2, 3], **{"four": 4, "five": 5})
-# **wont work**
-# echoKwargsOnly(*[1, 2, 3], **{"four": 4, "five": 5})
-#
-print '>> echoKwargsOnly(**{"four": 4, "five": 5})'
-echoKwargsOnly(**{"four": 4, "five": 5})
-
-print
-print "---- Example: calling arguments with an array or a dict ----"
-print
+def test_splat_args():
+    assert echo(*[1, 2, 3], **{"four": 4, "five": 5}) == ""
+    
+def test_splat_mismatch():
+    # **wont work**
+    # echoKwargsOnly(*[1, 2, 3], **{"four": 4, "five": 5})
+    #
+    assert echoKwargsOnly(**{"four": 4, "five": 5}) == ""
 
 def f1(arg1):
-    print arg1
+    return arg1
 
 def f2(arg1, arg2):
-    print arg1, arg2
+    return (arg1, arg2)
 
-print ">>f1(**{ 'arg1': 1 })"
-f1(**{ 'arg1': 1 })
+def test_dict():
+    """---- Example: calling arguments with an array or a dict ----"""
+    assert f1(**{ 'arg1': 1 }) == ""
 
 '''  
    Defining regular functions
 '''
 
 def info(object, spacing=10, collapse=1):
-    print "object: ", object
-    print "spacing: ", spacing
-    print "collapse: ", collapse
+    return {
+        "object": str(object),
+        "spacing": str(spacing),
+        "collapse": str(collapse)
+    }
 
 
-print "function name: %s" % info.__name__
+def test_func_name():
+    assert info.__name__ == ""
 
 '''
-   Lambda functions
+   Parameters
 '''
 
-#  Variation 1
-def f1(x):
-    return x*2
 
-#  Variation 2
-g = lambda x: x*2
+def test_extra_formal_args_with_lamba():
+    """ passing extra parameters to function with fixed arguments """ 
+    with pytest.raises(TypeError) as exc_info:
+        fixed_arg_lambda = lambda x: x*2
+        fixed_arg_lambda(1,2)
+    assert str(exc_info.value) == '<lambda>() takes 1 positional argument but 2 were given'
+
+def test_extra_formal_args_with_func():
+    """ passing extra parameters to lambda with fixed arguments """ 
+    with pytest.raises(TypeError) as exc_info:
+        def fixed_arg_func(x):
+            return x*2
+
+        fixed_arg_func(1,2)
+    assert str(exc_info.value) == 'fixed_arg_func() takes 1 positional argument but 2 were given'
+
+def test_extra_kw_args_with_lamba():
+    """ passing extra parameters to function with fixed arguments """ 
+    with pytest.raises(TypeError) as exc_info:
+        fixed_arg_lambda = lambda x: x*2
+        fixed_arg_lambda(1,y=2)
+    assert str(exc_info.value) == "<lambda>() got an unexpected keyword argument 'y'"
 
 
-import pdb;pdb.set_trace()
+def test_extra_mixed_args_with_lamba():
+    mixed_arg_lamba = lambda x, y=None: x + y if y else x
+
+    assert mixed_arg_lamba(1) == 1
+    assert mixed_arg_lamba(1, 2) == 3
+    
+    
+def num_args(func):
+    import inspect
+    if hasattr(inspect, 'getargspec'):
+        return len(inspect.getargspec(func).args)
+    else:
+        return len(inspect.signature(func).parameters)
+
+def test_inspect_args():
+    """ test passing in the right number of parameters """
+    def one_arg_func(x):
+        return x
+
+    def two_arg_func(x, y):
+        return x + y
+
+    assert num_args(one_arg_func) == 1
+    assert num_args(two_arg_func) == 2
+    
+    
+    
+
 
